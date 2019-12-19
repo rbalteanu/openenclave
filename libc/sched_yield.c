f8350ecf33270689486dc4aa3067ec7790ce07b2
Mike Brasher <mikbras@microsoft.com>
2019-12-18 21:28:18 -0600

    remove unused oe_sched_yield

--- libc/sched_yield.c
+++ libc/sched_yield.c
@@ -1,9 +1,17 @@
 // Copyright (c) Open Enclave SDK contributors.
 // Licensed under the MIT License.
 
-#include <openenclave/internal/core/sched.h>
+#include <sched.h>
 
 int sched_yield(void)
 {
-    return oe_sched_yield();
+    /* Since this is called by __cxa_guard_acquire() from
+       3rdparty/libcxxrt/libcxxrt/src/guard.cc and
+       std::this_thread::yield(), this routine needs to be supported for
+       std::thread multi-threading to work. Adding a pause before
+       returning 0 as a pause instruction is a hint to the CPU to improve
+       power and performance of spin-wait loops.
+     */
+    asm volatile("pause");
+    return 0;
 }
